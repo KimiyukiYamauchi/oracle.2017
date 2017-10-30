46b3c8ecbb294a734d8124caeeb95620a790698b
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2017-10-30 20:54:50 +0900

    日付書式の変更スクリプト

---
+++ c_date.sql
@@ -0,0 +1,2 @@
+alter session set nls_date_format = 'DD-MON-RR';
+alter session set nls_language = 'AMERICAN';
---
+++ r_date.sql
@@ -0,0 +1,2 @@
+alter session set nls_date_format = 'RR-MM-DD';
+alter session set nls_language = 'JAPANESE';
