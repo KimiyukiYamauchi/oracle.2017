c06a23ad90373b679ca65fc3f267b4c3a973f158
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2017-11-09 16:05:55 +0900

    参照先の制約を追加

--- cons.sql
+++ cons.sql
@@ -1,9 +1,13 @@
-col constraint_name for a20
+-- col constraint_name for a20
 col table_name for a20
 col column_name for a20
 col search_condition for a20
-select table_name, column_name, constraint_name, constraint_type, search_condition
+col r_constraint_name for a20
+-- col r_owner for a20
+select column_name, constraint_name, constraint_type, 
+search_condition, r_constraint_name
 from user_constraints
 natural join user_cons_columns
-where table_name = '&table_name'
+where table_name = upper('&table_name')
+order by column_name
 /
