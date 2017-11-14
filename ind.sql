d82c598106b9d4e7959e5f3d997e87af3c1dedf2
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2017-11-14 15:27:46 +0900

    インデックスが設定されて列名を表示

--- ind.sql
+++ ind.sql
@@ -1,4 +1,7 @@
-select index_name
+col column_name for a20
+col index_name for a20
+select column_name, index_name
 from user_indexes
+natural join user_ind_columns
 where table_name = upper('&table_name')
 /
