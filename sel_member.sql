5af68057870648ec965530019341fe60977fcf9e
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2017-11-15 15:21:34 +0900

    11/14演習確認用

---
+++ sel_member.sql
@@ -0,0 +1,8 @@
+col member_id for 9999
+col last_name for a10
+col first_name for a10
+col address for a10
+col city for a10
+col phone for a10
+select * from member
+/
---
+++ sel_title.sql
@@ -0,0 +1,5 @@
+col title_id for 9999
+col title for a10
+col description for a20
+select * from title
+/
